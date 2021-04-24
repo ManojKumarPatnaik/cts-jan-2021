20b3068c7ccdb5125cd2863c541eddcc7900caaa
Kishor <kishor1124@gmail.com>
2021-04-24 18:22:46 +0530

    reactive forms

--- Angular Projects/data-binding-app/src/app/mdf-demo/mdf-demo.component.ts
+++ Angular Projects/data-binding-app/src/app/mdf-demo/mdf-demo.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-mdf-demo',
   templateUrl: './mdf-demo.component.html',
   styleUrls: ['./mdf-demo.component.css']
 })
-export class MdfDemoComponent implements OnInit {
+export class MdfDemoComponent   {
 
-  constructor() { }
+  constructor(private form : FormBuilder) { }
 
-  ngOnInit(): void {
+  user = this.form.group({
+    firstname : ['', Validators.required],
+    lastname : ['', Validators.required]
+  });
+
+  handleSubmit() {
+    console.log(this.user.value);
+    //this.user.reset();
+    this.user.setValue({firstname :"", lastname: "Hello"})
   }
-
 }
